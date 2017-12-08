0ef9705787ce04b9fcde1278ba6b2d1defab2699
Francisco <fwmcomputer@gmail.com>
2017-12-08 20:19:46 +0100

    Primer commit

---
+++ Unity/Assets/Scripts/Carta.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carta : MonoBehaviour {
+
+    private int tipoCarta;
+    private string rutaImatge;
+
+	// Use this for initialization
+	void Start () {
+        tipoCarta = Global.TIPO_CARTA.Fuego;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Unity/Assets/Scripts/Global.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global {
+
+    public static class TIPO_CARTA
+    {
+        public static int Fuego = 0;
+        public static int Tierra = 1;
+        public static int Agua = 2;
+        public static int Electrica = 3;
+        public static int Fusion = 4;
+        public static int Magica = 5;
+        public static int Ancestral = 6;
+    }
+	
+}
